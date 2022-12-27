11935d845a06be080bf1d64fba641a40a6e6fb80
Junhyung Choi <junhyung9571@naver.com>
2022-12-27 17:44:57 +0900

    Feat: Login, Logout

--- MyProject/MyProject/urls.py
+++ MyProject/MyProject/urls.py
@@ -52,7 +52,7 @@ urlpatterns = [
     path("faq/update/", views.update_faq),
     path("faq/delete/", views.delete_faq),
 
-    path('dj-rest-auth/',include('dj_rest_auth.urls')),
-    path('dj-rest-auth/register/',include('dj_rest_auth.registration.urls'))
+    path('auth/',include('dj_rest_auth.urls')),
+    path('auth/register/',include('dj_rest_auth.registration.urls'))
 ] 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
